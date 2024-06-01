752077f89c6b3f85de36d312b34c4c0271905e32
Adibkhan619 <work.adibkhan@gmail.com>
2024-06-01 22:23:00 +0600

    Added make admin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app= express()
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middleware
 app.use(cors())
@@ -24,20 +24,65 @@ const client = new MongoClient(uri, {
       // Connect the client to the server	(optional starting in v4.7)
     //   await client.connect();
 
-        // * DATABASE COLLECTIONS --------------->
+        //  DATABASE COLLECTIONS --------------->
         const userCollection = client.db('surveyDB').collection('users')
 
-        // *GET ALL USER DATA ---------->
+        // GET ALL USER DATA ---------->
         app.get("/users", async(req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
 
+        // Post user data ---------->
         app.post("/users", async(req, res) =>{
             const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({
+                    message: "user already exist",
+                    insertedId: null,
+                });
+            }
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
+
+        // make admin ------------->
+        app.patch("/users/admin/:id", async (req, res) => {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) };
+                const updateDoc = {
+                    $set: {
+                        role: "admin",
+                    },
+                };
+                const result = await userCollection.updateOne(
+                    filter,
+                    updateDoc
+                );
+                res.send(result);
+            }
+        );
+
+        // Make surveyor ------------- >
+        app.patch("/users/surveyor/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: "surveyor",
+                },
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc
+            );
+            res.send(result);
+        }
+    );
+
+
 
 
       // Send a ping to confirm a successful connection
